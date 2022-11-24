1befa77508260612b3795dc3d62a5752e2027936
alanalzi <117884402+alanalzi@users.noreply.github.com>
2022-11-24 13:23:18 +0700

    Add files via upload

--- soal 1.py
+++ soal 1.py
@@ -11,11 +11,16 @@ print("")
 bangun_ruang = input("Masukan pilihan Anda ")
 if bangun_ruang == '1':
 #limas
-    sisi_alas = (input("Masukan sisi alas limas: "))
-    tinggi_limas = (input("Masukan tinggi limas: "))
-    hasil = str(sisi_alas * tinggi_limas/3)
+    sisi_alas = int(input("Masukan sisi alas limas: "))
+    tinggi_limas = int(input("Masukan tinggi limas: "))
+    hasil = float((sisi_alas*sisi_alas/3)*tinggi_limas)
     print("volume limas tersebut adalah",hasil)
 elif bangun_ruang == '2':
     jari_jari = int(input("Masukan panjang jari-jari bola: "))
     hasil_bola = int(4/3*3.14*jari)
     volume = print("Volume bola tersebut adalah",hasil)    
+elif bangun_ruang == '3':
+    print("Pilihlah salah satu dari pilihan di bawah:")
+    print("1. Prisma segitiga")
+    print("2. Prisma segiempat")
+    print("3. prisma segilima")

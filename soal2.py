15c930ed25091f0654cc6453fe27c0db7a7ffe85
alanalzi <117884402+alanalzi@users.noreply.github.com>
2022-11-24 13:05:46 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,4 @@
+print("========== PROGRAM PENGHITUNG PYTHAGORAS ==========")
+a = (input("Masukan bilangan bulat pertama: "))
+b = (input("Masukan bilangan bulat kedua: "))
+c = (input("Masukan bilangan bulat ketiga: "))
